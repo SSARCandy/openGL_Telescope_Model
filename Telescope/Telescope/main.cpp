55423a4407d5c8e2a6c52f5dab28a1120e8f222b
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2015-05-28 17:23:10 +0800

    Initial Commit

---
+++ Telescope/Telescope/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Telescope
+//
+//  Created by SSARCandy on 2015/5/28.
+//  Copyright (c) 2015年 SSARCandy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
